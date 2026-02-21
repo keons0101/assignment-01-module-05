a7e9985da093469d15c14e6a144e7896fc564a75
Santiagoo <santiagominotta2913@hotmail.com>
2026-02-21 00:39:49 -0800

    Add associative books array

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+$books = [
+    [
+        'name' => 'Joyland',
+        'author' => 'Stephen King',
+        'price' => 20.99,
+        'year' => 2013,
+        'category' => 'Mistery'
+    ],
+    [
+        'name' => 'The Chain',
+        'author' => 'Adrian McKinty',
+        'price' => 18.75,
+        'year' => 2019,
+        'category' => 'Thriller'
+    ],
+    [
+        'name' => 'Gone Girl',
+        'author' => 'Gillian Flynn',
+        'price' => 16.50,
+        'year' => 2012,
+        'category' => 'Mistery'
+    ],
+    [
+        'name' => 'The Silent Patient',
+        'author' => 'Alex Michaelides',
+        'price' => 19.40,
+        'year' => 2019,
+        'category' => 'Thriller'
+    ],
+    [
+        'name' => 'Dune',
+        'author' => 'Frank Herbert',
+        'price' => 24.99,
+        'year' => 1965,
+        'category' => 'Science Fiction'
+    ],
+    [
+        'name' => 'Neuromancer',
+        'author' => 'William Gibson',
+        'price' => 17.30,
+        'year' => 1984,
+        'category' => 'Science Fiction'
+    ],
+    [
+        'name' => 'The Girl with the Dragon Tattoo',
+        'author' => 'Stieg Larsson',
+        'price' => 21.10,
+        'year' => 2005,
+        'category' => 'Mistery'
+    ],
+    [
+        'name' => 'The Da Vinci Code',
+        'author' => 'Dan Brown',
+        'price' => 15.99,
+        'year' => 2003,
+        'category' => 'Thriller'
+    ],
+    [
+        'name' => 'Foundation',
+        'author' => 'Isaac Asimov',
+        'price' => 22.80,
+        'year' => 1951,
+        'category' => 'Science Fiction'
+    ],
+];
